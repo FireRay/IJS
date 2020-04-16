4339d09b0c6a42ab1e2f22e4cb20a94756f592e9
Jonas H <jonashoglund282@gmail.com>
2020-04-16 11:46:05 +0200

    Uploaded files
    Additional upload of files

---
+++ www/Board.js
@@ -0,0 +1,20 @@
+class Board {
+
+  constructor(game) {
+
+
+
+  }
+
+  makeMove(column) {
+
+
+
+  }
+
+
+
+}
+
+// make it possible to test on backend
+if (typeof global !== 'undefined') { global.Board = Board };
