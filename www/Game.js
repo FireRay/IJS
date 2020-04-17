bd4a4b4a32f788020280582093c94177f7b7f4c5
FireRay <jonashoglund282@gmail.com>
2020-04-17 16:17:08 +0200

    method that grabs DOM element to red if 1 and grabs DOM element to yellow if 2.

--- www/Game.js
+++ www/Game.js
@@ -2,6 +2,7 @@ class Game {
 
   constructor() {
 
+    //call methods in following order.
     addEventListener();
     start();
 
@@ -9,13 +10,26 @@ class Game {
 
   start() {
 
+    //create new instance of board and sends current instance of Game to it's constructor. 
+    //stores instance of Game in board var.
     let board = new Board(this);
 
   }
 
+
   tellTurn(player) {
 
+    //methods that receives inagrument 1 or 2 in player
+    //if value is 1 message changes it's content value to reds turn 
+    //if value is 2 message changes it's content value to yellows turn 
+    (player === 1) ? $('.message').innerHTML('Röds tur...') : (player === 2) ? $('.message').innerHTML('Guls tur...') : ('');
+
+
   }
+
+
+
+
 
   over(won) {
 
