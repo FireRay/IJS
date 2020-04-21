697a3fa3c3a3e50ffbc8f1147519d545d0e3d54e
Soren-S <soren.v.stahl@gmail.com>
2020-04-21 20:52:28 +0200

    over() test skelett

--- features/play-game.feature
+++ features/play-game.feature
@@ -6,14 +6,37 @@ Feature: Play Connect 4
   # Some random scenarios (out of the many needed)
   # (these have no When - but there will be plenty that have)
 
+  # 1
   Scenario: A new Game creates a new board
     Given that a new Game is created
     Then it should create a new Board
 
+  # 2
   Scenario: A board adds 42 divs to the .board element
     Given that a new Board is created
     Then it should render 42 divs as children of the board element
 
+  # 3
   Scenario: The .message element should display who's turn it is
     Given that a game is still in play
     Then the .message element should display a text of who's turn it is
+
+  # 4
+  Scenario: The player argument must be equal to 1 or 2
+    Given that a game is still in play
+    Then player must equal 1 or 2
+
+  # 5
+  Scenario: Draw message
+    Given that the game ends in a draw
+    Then the .message element displays `Det blev oavgjort!`
+
+  # 6
+  Scenario: Red wins message
+    Given that red has won
+    Then the .message element displays `Röd vann!`
+
+  # 7
+  Scenario: Yellow wins message
+    Given that yellow has won
+    Then the .message element displays `Gul vann!`
--- step-definitions/play-game.js
+++ step-definitions/play-game.js
@@ -8,6 +8,7 @@ module.exports = function () {
   let game;
 
 
+  // # 1
   this.Given(/^that a new Game is created$/, function () {
     game = new Game();
   });
@@ -18,6 +19,8 @@ module.exports = function () {
     );
   });
 
+
+  // # 2
   this.Given(/^that a new Board is created$/, function () {
     // Empty the contents of .board
     $$('.board').innerHTML = '';
@@ -32,6 +35,8 @@ module.exports = function () {
     );
   });
 
+
+  // # 3
   this.Given(/^that a game is still in play$/, function () {
     // Write code here that turns the phrase above into concrete actions
     expect(playInProgress).to.be.true;
@@ -39,14 +44,56 @@ module.exports = function () {
 
   this.Then(/^the \.message element should display a text of who's turn it is$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    if (player = 1) {
-      game.tellTurn(player);
+    if (currentPlayer = 1) {
       expect($('.message').innerHTML).to.equal('Röds tur…');
     }
-    else if (player = 2) {
-      game.tellTurn(player);
+    else if (currentPlayer = 2) {
       expect($('.message').innerHTML).to.equal('Guls tur…');
     }
   });
+
+
+  // # 4
+  this.Then(/^player must equal (\d+) or (\d+)$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    expect(currentPlayer).to.be.oneOf([1, 2])
+  });
+
+
+  // # 5 - SKELETT
+  this.Given(/^that the game ends in a draw$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    ; // kod här
+  });
+
+  this.Then(/^the \.message element displays `Det blev oavgjort!`$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    expect($('.message').innerHTML).to.equal('Det blev oavgjort!');
+  });
+
+
+  // # 6 - SKELETT
+  this.Given(/^that red has won$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    ; // kod här
+  });
+
+  this.Then(/^the \.message element displays `Röd vann!`$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    expect($('.message').innerHTML).to.equal('Röd vann!');
+  });
+
+
+  // # 7 - SKELETT
+  this.Given(/^that yellow has won$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    ; // kod här
+  });
+
+  this.Then(/^the \.message element displays `Gul vann!`$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    expect($('.message').innerHTML).to.equal('Gul vann!');
+  });
+
 
 }
