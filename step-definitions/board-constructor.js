9f1edcd26bd1bd7b95f541aeda8c9bc845157630
FireRay <jonashoglund282@gmail.com>
2020-04-24 16:28:15 +0200

    Changes in test for constructor Game, Board.

--- step-definitions/board-constructor.js
+++ step-definitions/board-constructor.js
@@ -2,27 +2,15 @@ require('./_include-all')();
 
 module.exports = function () {
 
-  let testGame;
-  let carl;
 
-
-  class TestClass extends Game {
-
-    start() {
-      carl = new Board('JOnas');
-
-    }
-
-
-
-  }
+  let runGame;
 
 
 
 
   this.Given(/^that the constructor recieves an argument$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    new TestClass();
+    runGame = new Game();
 
   });
 
@@ -30,7 +18,7 @@ module.exports = function () {
 
   this.Then(/^the argument should be an instance of Game$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    expect(carl.game).to.be.instanceof(Game, '');
+    expect(runGame.board.game).to.be.instanceof(Game, '');
 
   });
 
