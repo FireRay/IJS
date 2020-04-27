93f9f8d649e185f92539f969bd1fc607be352902
LadyQuinn <kiaa887@hotmail.com>
2020-04-27 23:54:25 +0200

    added wincheck

--- www/Board.js
+++ www/Board.js
@@ -39,8 +39,26 @@ class Board {
 
 
   winCheck() {
-
-
+    let winner = {
+      vinnaren: '',
+      combo: [ [], [], [], [] ]
+    }
+    let winOffset = [
+    [[0, 0], [0, 1], [0, 2] [0, 3]], // horisontal
+    [[0, 0], [1, 0], [2, 0], [3, 0]], // vertical 
+    [[0, 0], [1, -1], [2, -2], [3, -3]], // diagonal 1
+    [[0, 0], [1, -1], [2, -2], [3, -3]] // diagonal 2
+    ];
+      for (let row = 0; row < 6; row++) {
+        for (let column = 0; column < 7; column++){
+          for (let w of winOffset) {
+            let slots = w.map(([r, c]) => this.matrix[row + r] && this.matrix [row + r] [col + c]).join('');
+            if (slots === '1111' || slots === '2222') {
+              return +slots[0];
+            } //en bit saknas i koden, att kolla om det har blivit oavgjort, isåfall ska wincheck resonera draw
+          }
+      }
+    }
 
   }
 
