ff662b3803c66abfb328ab7e492a94d9e3fe249c
FireRay <jonashoglund282@gmail.com>
2020-04-28 18:31:44 +0200

    document.createElement added for test backend

--- step-definitions/_include-all.js
+++ step-definitions/_include-all.js
@@ -1,11 +1,14 @@
 // emulate helpers in backend
 const jsdom = require("jsdom");
 module.exports = function includeAll() {
-  const { JSDOM } = jsdom;
+  const {
+    JSDOM
+  } = jsdom;
   const DOM = new JSDOM('<!DOCTYPE html><div class="board"></div><div class="message"></div>');
   global.sleep = (ms) => new Promise(res => setTimeout(res, ms));
   global.$ = (x) => DOM.window.document.querySelector(x);
   global.$$ = (x) => DOM.window.document.querySelectorAll(x);
+  global.document = DOM.window.document; // ADDED LINE
   // import the classes (that will add themselves to global)
   require('../www/Game.js');
   require('../www/Board.js');
