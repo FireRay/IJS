108bd1a615bc41d25298d72f33d73eac387333fc
Soren-S <soren.v.stahl@gmail.com>
2020-05-05 10:27:35 +0200

    Board.winCheck() tester done

--- features/board-winCheck.feature
+++ features/board-winCheck.feature
@@ -4,17 +4,17 @@ Feature: A method that checks if the board has won
   So I crush my enemies
   # 1
   Scenario: There is a winner
-    Given that the player 1 or 2 has won
+    Given that the a player has won
     Then winCheck() returns an object with the winner and combo attribute
   # 2
   Scenario: Winner is 1 or 2
     Given that someone has won
-    Then the winner attribute of the winCheck() return value should be 1 or 2
+    Then the winner attribute of the winCheck() return value should be one / two
   # 3
   Scenario: The game ties
     Given that the outcome of the game is a tie
     Then the winner attribute of the winCheck() return value should be 'draw'
   # 4
   Scenario: The game is still going on
-    Given that neither of player 1 or 2 wins or the game is a draw
+    Given that neither of player has won and the game isn't a tie
     Then winCheck() should return the value false
--- step-definitions/board-winCheck.js
+++ step-definitions/board-winCheck.js
@@ -2,47 +2,77 @@ require('./_include-all')();
 
 module.exports = function () {
 
+  let testGame;
+  let results;
   // # 1
-  this.Given(/^that the player (\d+) or (\d+) has won$/, function () {
+  this.Given(/^that the a player has won$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    ;
+    testGame = new Game();
+    testGame.board.matrix = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0, 0, 0]
+    ];
   });
 
   this.Then(/^winCheck\(\) returns an object with the winner and combo attribute$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    ;
+    results = testGame.board.winCheck();
+    expect(results).to.have.property('winner');
+    expect(results).to.have.property('combo');
   });
 
   // # 2
   this.Given(/^that someone has won$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+    // No need to add anything here from last Given 
   });
 
-  this.Then(/^the winner attribute of the winCheck\(\) return value should be (\d+) or (\d+)$/, function () {
+  this.Then(/^the winner attribute of the winCheck\(\) return value should be one \/ two$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    ;
+    expect(results.winner).to.be.oneOf([1, 2]);
   });
 
   // # 3
   this.Given(/^that the outcome of the game is a tie$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    ;
+    testGame.board.matrix = [
+      [2, 1, 2, 1, 2, 1, 2],
+      [1, 1, 2, 1, 2, 1, 1],
+      [2, 2, 1, 2, 1, 2, 2],
+      [1, 1, 2, 1, 2, 1, 1],
+      [2, 1, 2, 1, 2, 1, 2],
+      [1, 1, 1, 2, 1, 1, 1]
+    ];
   });
+
 
   this.Then(/^the winner attribute of the winCheck\(\) return value should be 'draw'$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    ;
+    results = testGame.board.winCheck();
+    expect(results.winner).to.be.equal('draw');
   });
 
   // # 4
-  this.Given(/^that neither of player (\d+) or (\d+) wins or the game is a draw$/, function () {
+  this.Given(/^that neither of player has won and the game isn't a tie$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    ;
+    testGame.board.matrix = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0]
+    ];
   });
 
   this.Then(/^winCheck\(\) should return the value false$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    ;
+    results = testGame.board.winCheck();
+    expect(results).to.be.false;
   });
+
 }
