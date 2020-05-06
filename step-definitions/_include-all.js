416233aa9941586542bf1f1abc6b4f4015d912c1
Soren-S <soren.v.stahl@gmail.com>
2020-05-06 19:20:49 +0200

    format include all

--- step-definitions/_include-all.js
+++ step-definitions/_include-all.js
@@ -1,9 +1,7 @@
 // emulate helpers in backend
 const jsdom = require("jsdom");
 module.exports = function includeAll() {
-  const {
-    JSDOM
-  } = jsdom;
+  const { JSDOM } = jsdom;
   const DOM = new JSDOM('<!DOCTYPE html><div class="board"></div><div class="message"></div>');
   global.sleep = (ms) => new Promise(res => setTimeout(res, ms));
   global.$ = (x) => DOM.window.document.querySelector(x);
