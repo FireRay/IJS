d1debbee9a9291cb7f50030e45661c157d893fac
Soren-S <soren.v.stahl@gmail.com>
2020-05-06 19:12:59 +0200

    over bug fix

--- www/Game.js
+++ www/Game.js
@@ -24,8 +24,8 @@ class Game {
   }
 
   over(won) {
-
-    if (!(won === 1 || won === 2 || won === 'draw')) {
+    console.log(won)
+    if (!(won == 1 || won == 2 || won === 'draw')) {
       throw 'won must be “draw”, 1 or 2';
     }
 
