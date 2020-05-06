586912d6535c34cb377b702a49fc1501c14cff8e
FireRay <jonashoglund282@gmail.com>
2020-05-06 16:57:27 +0200

    changes in board-makeMove-possible

--- features/board-makeMove-possible.feature
+++ features/board-makeMove-possible.feature
@@ -4,7 +4,7 @@ Feature: When making a move is possible
   So I can play the game
   # 1
   Scenario: Set playInProgress to true
-    Given that the move is possible
+    Given that the move possible
     Then playInProgress should be set to true
   # 2
   Scenario: Call method render
@@ -12,21 +12,21 @@ Feature: When making a move is possible
     Then the board method render should be called
   # 3
   Scenario: Set currentPlayer to one
-    Given the move was possible and that currentPlayer is two
+    Given the move was possible and that currentPlayer is one
     Then currentPlayer should be set to one
   # 4
   Scenario: Set currentPlayer to two
     Given the move was possible and that currentPlayer is two
     Then currentPlayer should be set to one
-  # 4
+  # 5
   Scenario: Call tellTurn
     Given that the move was possible
     Then the tellTurn method should be called with the currentPlayer as argument
-  # 5
-  Scenario: Set playInProgress to false
-    Given that the move was possible
-    Then it should set playInProgress to true
   # 6
+  Scenario: Set playInProgress to false
+    Given that the move is possible
+    Then it should set playInProgress to false
+  # 7
   Scenario: Return true
     Given that winCheck wasn't truthy
     Then in the end makeMove returns true
--- step-definitions/board-makeMove-possible.js
+++ step-definitions/board-makeMove-possible.js
@@ -7,6 +7,18 @@ module.exports = function () {
   let runGame;
   let startRender = false;
 
+  class testGame extends Game {
+
+
+    start()
+
+    {
+      this.board = new testClass();
+    }
+
+
+  }
+
 
   class testClass extends Board {
 
@@ -14,48 +26,45 @@ module.exports = function () {
     constructor(game) {
       super(game);
 
-      this.render()
-
-      {
-        startRender = true;
-      }
-
 
     }
 
+    render()
+
+    {
+      startRender = true;
+    }
 
 
   }
 
 
 
-
-  //# 1
-
-  this.Given(/^that the move is possible$/, function () {
+  // # 1
+  this.Given(/^that the move possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
+
+    runBoard = new Board();
 
   });
 
-  //# 1
 
+  // # 1
   this.Then(/^playInProgress should be set to true$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    runBoard = new Board();
-    expect(runBoard.makeMove.playInProgress).to.equal(true);
-
+    expect(runBoard.makeMove.playInProgress).to.be.true;
   });
 
 
-  //# 2
+  // # 2
   this.Given(/^that makeMove is possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    runTestBoard = new testClass();
-    expect(makeMove()).to.not.throw();
-
+    runGame = new testGame();
+    runGame.board.makeMove(3);
   });
 
-  //# 2
+
+  // # 2
   this.Then(/^the board method render should be called$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
@@ -64,32 +73,49 @@ module.exports = function () {
   });
 
 
-  //# 3
-  this.Given(/^the move was possible and that currentPlayer is two$/, function () {
+  // # 3
+  this.Given(/^the move was possible and that currentPlayer is one$/, function () {
     // Write code here that turns the phrase above into concrete actions
+    runGame = new Game();
+    runGame.board.currentPlayer = 1;
+    runGame.board.makeMove(4);
 
 
   });
 
 
-  //# 3
+  // # 3
   this.Then(/^currentPlayer should be set to one$/, function () {
     // Write code here that turns the phrase above into concrete actions
+    expect(runGame.board.currentPlayer).to.eql(2);
+  });
 
-    expect(runBoard.makeMove.currentPlayer == 1).to.eql(2);
+
+  // # 4
+  this.Given(/^the move was possible and that currentPlayer is two$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    runGame = new Game();
+    runGame.board.currentPlayer = 2;
+    runGame.board.makeMove(4);
 
   });
 
 
+  // # 4
+  this.Then(/^currentPlayer should be set to one$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    expect(runGame.board.currentPlayer).to.eql(1);
 
-  //# 4
+  });
+
+
+  // # 5
   this.Given(/^that the move was possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-
-  //# 4
+  // # 5
   this.Then(/^the tellTurn method should be called with the currentPlayer as argument$/, function () {
     // Write code here that turns the phrase above into concrete actions
     runGame = new Board();
@@ -100,36 +126,40 @@ module.exports = function () {
 
   });
 
-  //# 5
 
-  this.Given(/^that the move was possible$/, function () {
+  // # 6
+  this.Given(/^that the move is possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  //# 5
-  this.Then(/^it should set playInProgress to true$/, function () {
+  // # 6
+
+  this.Then(/^it should set playInProgress to false$/, function () {
     // Write code here that turns the phrase above into concrete actions
     runBoard = new Board();
     expect(runBoard.makeMove.playInProgress).to.equal(true);
 
-
   });
 
 
-  //# 6
+  // # 7
   this.Given(/^that winCheck wasn't truthy$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-  ///# 6
+
+  // # 7
   this.Then(/^in the end makeMove returns true$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
-
-
-
   });
+
+
+
+
+
+
 
 }
