45ae80a2da2235bb18409b00aee13cff65e81510
FireRay <jonashoglund282@gmail.com>
2020-05-06 15:03:45 +0200

    Changes in board-makeMove-possible

--- step-definitions/board-makeMove-possible.js
+++ step-definitions/board-makeMove-possible.js
@@ -4,13 +4,14 @@ module.exports = function () {
 
 
 
+  //# 1
 
   this.Given(/^that the move is possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-
+  //# 1
 
   this.Then(/^playInProgress should be set to true$/, function () {
     // Write code here that turns the phrase above into concrete actions
@@ -18,27 +19,27 @@ module.exports = function () {
   });
 
 
-
+  //# 2
   this.Given(/^that makeMove is possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-
+  //# 2
   this.Then(/^the board method render should be called$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
 
-
+  //# 3
   this.Given(/^the move was possible and that currentPlayer is two$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
 
-
+  //# 3
   this.Then(/^currentPlayer should be set to one$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
@@ -46,52 +47,40 @@ module.exports = function () {
 
 
 
-  this.Given(/^the move was possible and that currentPlayer is two$/, function () {
-    // Write code here that turns the phrase above into concrete actions
-
-  });
-
-
-
-  this.Then(/^currentPlayer should be set to one$/, function () {
-    // Write code here that turns the phrase above into concrete actions
-
-  });
-
-
+  //# 4
   this.Given(/^that the move was possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
 
-
+  //# 4
   this.Then(/^the tellTurn method should be called with the currentPlayer as argument$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-
+  //# 5
 
   this.Given(/^that the move was possible$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-
+  //# 5
   this.Then(/^it should set playInProgress to true$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
 
-
+  //# 6
   this.Given(/^that winCheck wasn't truthy$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
   });
 
-
+  ///# 6
   this.Then(/^in the end makeMove returns true$/, function () {
     // Write code here that turns the phrase above into concrete actions
 
